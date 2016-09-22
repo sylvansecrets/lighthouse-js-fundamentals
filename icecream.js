690ea39d3fc8a51974f68c0eac6e9fd5226785cb
Richard Zhang <lapislacrima@gmail.com>
2016-09-22 03:31:00 +0000

    adds icecream.js, which corresponds to the icecream array exercise

---
+++ icecream.js
@@ -0,0 +1,15 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//Step 1
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+//Step 2
+console.log(iceCreamFlavours[0]);
+
+//Step 3
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
+
+//Step 4
+console.log(iceCreamFlavours.length)
